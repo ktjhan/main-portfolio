9bd2ce3e378a6463c34fedbab64954c282bd3272
ktjhan <tjhan.kavin@gmail.com>
2019-12-14 01:12:37 -0800

    Updates

---
+++ precache-manifest.b7c0f55e02db1764870fd017a82edf45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e75195f51a3ba93c961dcf9628a1b24f",
+    "url": "/main-portfolio/index.html"
+  },
+  {
+    "revision": "1825bb8dbe5c2265f6b1",
+    "url": "/main-portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "356096a4a5922e47127b",
+    "url": "/main-portfolio/static/js/2.80ad292e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/main-portfolio/static/js/2.80ad292e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1825bb8dbe5c2265f6b1",
+    "url": "/main-portfolio/static/js/main.0606ae91.chunk.js"
+  },
+  {
+    "revision": "86447631a74cc995abf2",
+    "url": "/main-portfolio/static/js/runtime-main.9aa1ed88.js"
+  }
+]);
